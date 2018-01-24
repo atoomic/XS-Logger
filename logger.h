5ac6134b1c4aab5682c24da8bbc876070c6319b1
Nicolas R <atoomic@cpan.org>
2018-01-24 16:22:30 -0600

    fix a bug where va_copy is required for vfprintf

--- logger.h
+++ logger.h
@@ -51,10 +51,29 @@ void do_log(MyLogger *mylogger, logLevel level, const char *fmt, int num_args, .
 #define ACQUIRE_LOCK_ONCE(f) if ( hold_lock == false ) { flock( fileno(f), LOCK_EX ); hold_lock = true; }
 #define RELEASE_LOCK(f) if ( hold_lock == true ) flock( fileno(f), LOCK_UN );
 
-/* https://gcc.gnu.org/onlinedocs/gcc/Variadic-Macros.html */
+/*
+	https://gcc.gnu.org/onlinedocs/gcc/Variadic-Macros.html
+
+	Some tricks:
+	https://codecraft.co/2014/11/25/variadic-macros-tricks/
+*/
+
 /* print on provided fh + stderr as a bonus */
-#define M_FPRINTF(fh, format, ...) { fprintf(fh, format, __VA_ARGS__); if (quiet == false) fprintf(stderr, format, __VA_ARGS__); }
-#define M_VFPRINTF(fh, format, args) { vfprintf(fh, format, args); if (quiet == false) vfprintf(stderr, format, args); }
-#define M_FPUTS(format, fh) { fputs( format, fh ); if (quiet == false) fputs( format, stderr ); }
+#define M_FPRINTF(fh, format, ...) { \
+	fprintf(fh, format, __VA_ARGS__); \
+	if (quiet == false) fprintf(stderr, format, __VA_ARGS__); \
+}
+
+#define M_VFPRINTF(fh, format, xargs) { \
+	va_list xargs2; \
+	if (quiet == false) va_copy(xargs2, xargs); \
+	vfprintf(fh, format, xargs); \
+	if (quiet == false) { vfprintf(stderr, format, xargs2); va_end(xargs2); } \
+}
+
+#define M_FPUTS(format, fh) { \
+	fputs( format, fh ); \
+	if (quiet == false) fputs( format, stderr ); \
+}
 
 #endif /* XS_LOGGER_H */
